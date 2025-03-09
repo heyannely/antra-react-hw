028caaa3ec54d5d3158b0ab90d5433960267e176
Anne Lee <36091029+heyannely@users.noreply.github.com>
2025-03-09 14:54:35 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Todo List</h1>
+      <div style={{ display: "flex", gap: "20px" }}>
+        <div>
+          <p>Pending Tasks</p>
+          <ul></ul>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
