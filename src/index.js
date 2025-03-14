71801a02a9bee4825f8dc1b6f8005a233d317a73
Anne Lee <36091029+heyannely@users.noreply.github.com>
2025-03-14 15:31:27 +0000

    inclue files update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.js"
+import App from "./App.js";
+import CustomHooksStorage from "./CustomHooksStorage.jsx";
+import TeslaClientHw from "./TeslaClientHw.jsx";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <CustomHooksStorage />
+    <TeslaClientHw />
   </StrictMode>
 );
